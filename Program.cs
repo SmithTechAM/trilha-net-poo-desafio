95f827fe45b9ee34ec127d77e5e135a96122ae43
Lucas Smith <lucas.smith@itriad.org.br>
2023-09-22 12:00:58 -0400

    Desafio Poo concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone apple = new Iphone("1234","14 pro max","DJ6NGJ1",256);
+apple.Ligar();
+apple.InstalarAplicativo("Instagram");
+
+Console.WriteLine("SmartPhone Nokia");
+Smartphone nokinha = new Nokia("5678","S87G","FGU6A4",64);
+nokinha.ReceberLigacao();
+nokinha.InstalarAplicativo("Whatsapp");
